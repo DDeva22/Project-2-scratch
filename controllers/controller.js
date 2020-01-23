d5b4377e76aa11cc56755f20fc82aca157177258
danman15726 <danman15726@gmail.com>
2020-01-22 19:11:48 -0600

    Commiting to move to the laptop

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,7 +4,19 @@ const router = express.Router();
 const db = require("../models");
 
 
+router.post("/login", function (req, res){
+    db.userBase.create({
+        legal_name: req.body.legalname,
+        user_name: req.body.username,
+        user_password: req.body.userpassword,
+        
 
+
+
+
+
+    });
+})
 
 
 
